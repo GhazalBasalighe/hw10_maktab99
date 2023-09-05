8a24bcf9a363756206487fb62ffa21901ea34f0e
Ghazal Basalighe <ghazalbasaligheh@gmail.com>
2023-09-06 02:41:02 +0330

    event delegation doesn't work for focus bc it doesn't bubble up . refactor applied

--- Q2/Q2.js
+++ Q2/Q2.js
@@ -1,16 +1,24 @@
 // SELECTING ELEMENTS WE NEED FROM HTML
-const weightInput = document.getElementById("weight");
-const heightInput = document.getElementById("height");
+const weightInput = document.querySelector("#weight");
+const heightInput = document.querySelector("#height");
 const submitButton = document.querySelector("button");
+
+// FUNCTION FOR FOCUS EVENT
+function backgroundColorChange(element) {
+  element.classList.remove("bg-secondary");
+  element.style.backgroundColor = "rgb(60, 60, 60)";
+}
 
 //ATTACHING EVENT LISTENERS TO EACH ELEMENT
 weightInput.addEventListener("focus", function () {
-  weightInput.classList.remove("bg-secondary");
-  weightInput.style.backgroundColor = "rgb(60, 60, 60)";
+  backgroundColorChange(weightInput);
+  heightInput.classList.add("bg-secondary");
+  heightInput.style.backgroundColor = "";
 });
 heightInput.addEventListener("focus", function () {
-  heightInput.classList.remove("bg-secondary");
-  heightInput.style.backgroundColor = "rgb(60, 60, 60)";
+  backgroundColorChange(heightInput);
+  weightInput.classList.add("bg-secondary");
+  weightInput.style.backgroundColor = "";
 });
 
 // REMOVING THE EVENT LISTENERS IF IT WAS CLICKED ON THE CONTAINER ITSELF
